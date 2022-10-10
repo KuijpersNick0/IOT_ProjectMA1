a7997a6e74a87e209405cfb94448e858795ab026
Nick Kuijpers <73790831+KuijpersNick0@users.noreply.github.com>
2022-10-10 10:44:43 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
+const mqtt = require('mqtt')
+var express = require('express');
+var router = express.Router();
+var url = require('url');
+
+const host = '127.0.0.1' 
+const port = '1883' 
+ 
 const connectUrl = `mqtt://${host}:${port}`
-const client = mqtt.connect(connectUrl, {
+const client = mqtt.connect(connectUrl, { 
     clean: true,
     connectTimeout: 4000,
-    username: 'ecam-dht11@ttn',
-    password: 'xxxxxx',
+    username: 'emqx',
+    password: 'public',
     reconnectPeriod: 1000,
 })
 
 client.on('connect', () => {
     var topic = "v3/ecam-dht11@ttn/devices/eui-70b3d57ed0055dbb/up";
+    var topic = "'/nodejs/mqtt'"
     console.log('Connected')
     client.subscribe(topic); //single topic
     console.log("Connected + subscribed");
 });
+
 //handle errors
 client.on("error", function (error) {
     console.log("Can't connect" + error);
     process.exit(1)
 });
+
 //handle incoming messages
 client.on("message", function (topic, message, packet) {
     var getDataFromTTN = JSON.parse(message);
@@ -25,3 +36,11 @@ client.on("message", function (topic, message, packet) {
     console.log("message is " + message);
     console.log("topic is " + topic);
 }) 
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    var config =  url.parse(connectUrl);
+    config.topic = topic;
+    res.render('../Views/homepage.ejs') 
+});
+ 
