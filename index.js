25ad19c6c5d67745531de5c9a0e68ad6aaca6c37
Nick Kuijpers <73790831+KuijpersNick0@users.noreply.github.com>
2022-10-10 11:57:02 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const mqtt = require('mqtt')
-var express = require('express');
-var router = express.Router();
+var express = require('express'); 
+let app = express();
+app.use(express.urlencoded({extended:true}));
+
 var url = require('url');
 
-const host = '127.0.0.1' 
+const host = 'eu1.cloud.thethings.network' 
 const port = '1883' 
- 
+
+let getDataFromTTN;
+
 const connectUrl = `mqtt://${host}:${port}`
 const client = mqtt.connect(connectUrl, { 
     clean: true,
     connectTimeout: 4000,
-    username: 'emqx',
-    password: 'public',
+    username: 'iot-group3@ttn',
+    password: 'NNSXS.E4RI4XRQL4HTRVGYELCKTAZYAXUW4FP5FM3C2EY.SJQH5N2EG7YMJ37MP56ZCZQUSIW2CZUX77QEWLUOPI7U4TFN7NDA',
     reconnectPeriod: 1000,
 })
 
 client.on('connect', () => {
-    var topic = "v3/ecam-dht11@ttn/devices/eui-70b3d57ed0055dbb/up";
-    var topic = "'/nodejs/mqtt'"
+    var topic = "v3/iot-group3@ttn/devices/eui-70b3d57ed0055dbb/up"; 
     console.log('Connected')
     client.subscribe(topic); //single topic
     console.log("Connected + subscribed");
@@ -31,16 +34,22 @@ client.on("error", function (error) {
 
 //handle incoming messages
 client.on("message", function (topic, message, packet) {
-    var getDataFromTTN = JSON.parse(message);
+    getDataFromTTN = JSON.parse(message);
     data = getDataFromTTN.uplink_message.decoded_payload;
     console.log("message is " + message);
     console.log("topic is " + topic);
 }) 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    var config =  url.parse(connectUrl);
-    config.topic = topic;
-    res.render('../Views/homepage.ejs') 
+app.get('/', function(req, res, next) {  
+    res.render('../Views/homepage.ejs',{getDataFromTTN: getDataFromTTN});
+});
+
+//setting middleware; css/img
+app.use(express.static('public'));
+
+
+app.listen(3000, function() {
+    console.log("le serveur running on port 3000")
 });
  
