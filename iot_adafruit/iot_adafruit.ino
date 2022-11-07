d76c774f8c62d99de6db722242b5f01d08083197
Chacha <79138550+ChachaPlusUltra@users.noreply.github.com>
2022-11-07 10:57:24 +0100

    transmitter code for LoRa

--- iot_adafruit/iot_adafruit.ino
+++ iot_adafruit/iot_adafruit.ino
@@ -1,4 +1,8 @@
 
+//Transmit via lora includes
+#include <SPI.h>
+#include <LoRa.h>
+
 // include the DHT Sensor Library
 #include "DHT.h"
 
@@ -10,17 +14,23 @@
 DHT dht(DHTPIN, DHTTYPE, 15);
 
 void setup() {
-  // put your setup code here, to run once:
   
    // Serial
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   // Init DHT 
   dht.begin();
+
+  while(!Serial);
+  Serial.println("LoRa Sender");
+
+  if(!LoRa.begin(433E6)){
+    Serial.println("Starting LoRa failed!");
+    while(1);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   
   // Reading temperature and humidity
   float humidity = dht.readHumidity();
@@ -28,16 +38,33 @@ void loop() {
   // Read temperature as Celsius
   float temperature = dht.readTemperature();
 
+  Serial.println("Sending packet: ");
+
+  //Send packet
+  LoRa.beginPacket();
+  
+  LoRa.print("Humidity: ");
+  LoRa.print(humidity);
+  LoRa.print("%");
+
+  LoRa.print("Temperature: ");
+  LoRa.print(temperature);
+  LoRa.print("C");
+
+  LoRa.endPacket();
+  delay(1000);
+  
   // Display
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+  
   Serial.print("Temperature: ");
   Serial.print(temperature);
   Serial.println(" C");
 
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.println(" %");
+  
 
   // Wait  
   delay(500);
-
 }
